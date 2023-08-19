5862e6b7a613ed068b9adf5510513f24f1873f8d
unknown <kepmiogokimiebi@gmail.com>
2023-08-19 18:31:42 +0200

    feat: Implement and document main.py for switch and host simulation.

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from switch.switch import Switch
 from host.host import Host
 
-
 def main():
+    """
+    The main function to demonstrate the simulation of switches and hosts.
+
+    This function creates instances of the Switch and Host classes, simulating a simple network environment.
+
+    """
     # Create switches and hosts
     switch1 = Switch("Switch1")
     host1 = Host("Host1")
 
-
 if __name__ == "__main__":
-    main()
+    """
+    Entry point of the script to demonstrate the switch logic.
+    
+    Creates an instance of the Switch class, processes a sample packet, and prints the MAC table.
+    """
+    switch = Switch("Switch1")
+    packet = {'src_mac': 'src_mac', 'dst_mac': 'dst_mac', 'msg_data': 'msg_data'}
+    switch.process_packet(packet)
+    switch.print_mac_table()
