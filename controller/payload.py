6350afc566172621877a75903e0c72bba0c8ee4b
unknown <kepmiogokimiebi@gmail.com>
2023-08-20 16:39:30 +0200

    feat: Implement install_flow_rule method with dynamic payloads.

---
+++ controller/payload.py
@@ -0,0 +1,18 @@
+
+install_flow_rule_payload = {
+    "match": {
+        "eth_src": None,
+        "eth_dst": None
+    },
+    "actions": [
+        {
+            "type": "output",
+            "port": None
+        }
+    ]
+}
+
+update_flow_rule_payload = {
+    "key1": None,
+    "key2": None
+}
